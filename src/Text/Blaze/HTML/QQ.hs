3fc9d23b0aea9ddd89fb33f55245dfa753bfd7fb
Marco Zocca <marco.zocca@unfoldml.com>
2023-04-10 21:22:27 +0200

    saving

--- src/Text/Blaze/HTML/QQ.hs
+++ src/Text/Blaze/HTML/QQ.hs
@@ -1,5 +1,6 @@
 {-# language OverloadedStrings #-}
 {-# language TemplateHaskell #-}
+{-# options_ghc -Wno-unused-imports #-}
 module Text.Blaze.HTML.QQ where
 
 import Data.String (IsString(..))
@@ -13,3 +14,103 @@ import Language.Haskell.TH.Quote (QuasiQuoter(..))
 import qualified Data.Text.Lazy as LT (Text)
 
 
+
+h0 :: LT.Text
+h0 = "<div class=\"col-12 uppercase\"><div class=\"uppercase\">Col-12</div></div>"
+
+
+{-
+<div class="row">
+    <div class="col-12 uppercase">
+        <div class="uppercase">Col-12</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-11 uppercase">
+        <div class="uppercase">Col-11</div>
+    </div>
+    <div class="col-1 uppercase">
+        <div class="uppercase">Col-1</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-10 uppercase">
+        <div class="uppercase">Col-10</div>
+    </div>
+    <div class="col-2 uppercase">
+        <div class="uppercase">Col-2</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-9 uppercase">
+        <div class="uppercase">Col-9</div>
+    </div>
+    <div class="col-3 uppercase">
+        <div class="uppercase">Col-3</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-8 uppercase">
+        <div class="uppercase">Col-8</div>
+    </div>
+    <div class="col-4 uppercase">
+        <div class="uppercase">Col-4</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-7 uppercase">
+        <div class="uppercase">Col-7</div>
+    </div>
+    <div class="col-5 uppercase">
+        <div class="uppercase">Col-5</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-6 uppercase">
+        <div class="uppercase">Col-6</div>
+    </div>
+    <div class="col-6 uppercase">
+        <div class="uppercase">Col-6</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-5 uppercase">
+        <div class="uppercase">Col-5</div>
+    </div>
+    <div class="col-7 uppercase">
+        <div class="uppercase">Col-7</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-4 uppercase">
+        <div class="uppercase">Col-4</div>
+    </div>
+    <div class="col-8 uppercase">
+        <div class="uppercase">Col-8</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-3 uppercase">
+        <div class="uppercase">Col-3</div>
+    </div>
+    <div class="col-9 uppercase">
+        <div class="uppercase">Col-9</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-2 uppercase">
+        <div class="uppercase">Col-2</div>
+    </div>
+    <div class="col-10 uppercase">
+        <div class="uppercase">Col-10</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-1 uppercase">
+        <div class="uppercase">Col-1</div>
+    </div>
+    <div class="col-11 uppercase">
+        <div class="uppercase">Col-11</div>
+    </div>
+</div>
+-}
